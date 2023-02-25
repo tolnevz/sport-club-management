5fa0aa53b19011be147808397ff1067a7d78a5d9
Anatoly Nevzorov <tolnevz@gmail.com>
2023-02-25 19:40:21 +0300

    angular settings fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sclub2'`, () => {
+  it(`should have as title 'sclub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sclub2');
+    expect(app.title).toEqual('sclub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sclub2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sclub app is running!');
   });
 });
